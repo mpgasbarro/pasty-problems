0e64df64f9536922d0579d462aa2281bfd533a29
Gaspy <mpgasbarro@gmail.com>
2021-01-20 15:32:37 -0500

    added small ajax code but do not have it working. Just foundation. Coming back to this tomorrow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import Home from './Home/Home';
 
-class App extends Component {
+const url1 = class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			apiUrl: {},
 		};
 	}
+
+	getUVIndex() {
+		var lat = $('#lat').val();
+		var lng = $('#lng').val();
+		var alt = $('#alt').val();
+		var ozone = $('#ozone').val();
+		var dt = $('#dt').val();
+
+		$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			beforeSend: function (request) {
+				request.setRequestHeader(
+					'x-access-token',
+					'7469fa7008ca4c729b71acc469c72468'
+				);
+			},
+			url:
+				'https://api.openuv.io/api/v1/uv?lat=' +
+				lat +
+				'&lng=' +
+				lng +
+				'&alt=' +
+				alt +
+				'&ozone=' +
+				ozone +
+				'&dt=' +
+				dt,
+			success: function (response) {
+				//handle successful response
+			},
+			error: function (response) {
+				// handle error response
+			},
+		});
+	}
+
 	render() {
 		return (
 			<div>
@@ -17,6 +54,6 @@ class App extends Component {
 			</div>
 		);
 	}
-}
+};
 
 export default App;
