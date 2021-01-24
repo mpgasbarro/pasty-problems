b3dae7e35c45369da37c7d63db3b5435536cd4ad
Gaspy <mpgasbarro@gmail.com>
2021-01-24 13:06:13 -0500

    env key is not working within UVINdex. Working on

--- src/UvIndex/UvIndex.js
+++ src/UvIndex/UvIndex.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
-class Userinput extends Component {
+class UVIndex extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputVal: '',
+			uvIndex: '',
 		};
 	}
 
+	componentDidMount() {
+		const url = `https://api.openuv.io/api/v1/uv?lat${this.props.lat}=&lng=${this.props.long}&dt=2018-01-24T10:50:52.283Z`;
+
+		console.log(url);
+
+		fetch(url, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json',
+				'x-access-token': `${process.env.REACT_APP_UV_API_KEY}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				this.setState({ uvIndex: res });
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+
 	render() {
-		console.log(this.props.lat);
+		console.log(this.state.uvIndex);
 		return (
 			<div>
 				<h1> hello! </h1>
@@ -18,4 +40,4 @@ class Userinput extends Component {
 	}
 }
 
-export default Userinput;
+export default UVIndex;
